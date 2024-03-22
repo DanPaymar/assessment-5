ae30503e7104b02e9bc8da69a211cf4b6601347d
arcticchimpanzee <152743640+arcticchimpanzee@users.noreply.github.com>
2024-03-22 16:23:43 -0700

    local repo

---
+++ server/index.js
@@ -0,0 +1,26 @@
+//
+
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+const { SERVER_PORT } = process.env
+const { seed, getCountries, getCities, createCity, deleteCity } = require('./controller.js')
+
+app.use(express.json())
+app.use(cors())
+
+// DEV
+app.post('/seed', seed)
+
+// COUNTRIES
+app.get('/countries', getCountries)
+
+// CITIES
+app.post('/cities', createCity)
+app.get('/cities', getCities)
+app.delete('/cities/:id', deleteCity)
+
+app.listen(SERVER_PORT, () => console.log(`toot toot, you're on the toot train to nowhere ${SERVER_PORT}`))
